0b4391a71259216ccdb3728ee7d58fe1e31185d1
pedro_ibs <pedro.igor.ifsp@gmail.com>
2021-06-28 22:42:09 -0300

    arrumando PersonVO

--- apiAcervo/src/main/java/br/edu/livros/acervo/data/vo/v1/PersonVO.java
+++ apiAcervo/src/main/java/br/edu/livros/acervo/data/vo/v1/PersonVO.java
@@ -6,80 +6,83 @@ import org.springframework.hateoas.RepresentationModel;
 
 import com.github.dozermapper.core.Mapping;
 
-
 public class PersonVO extends RepresentationModel implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
 	
 	@Mapping("id")
 	private Long key;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String password;
 	private String address;
 	private String gender;
 	
-	public PersonVO() {
-		
-	}
 	
 	public Long getKey() {
 		return key;
 	}
-	
-	public void setKey(Long id) {
-		this.key = id;
+	public void setKey(Long key) {
+		this.key = key;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
 		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
 		result = prime * result + ((key == null) ? 0 : key.hashCode());
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
@@ -88,6 +91,11 @@ public class PersonVO extends RepresentationModel implements Serializable {
 			if (other.address != null)
 				return false;
 		} else if (!address.equals(other.address))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
 			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
@@ -109,6 +117,15 @@ public class PersonVO extends RepresentationModel implements Serializable {
 				return false;
 		} else if (!lastName.equals(other.lastName))
 			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
 		return true;
-	}
+	}	
+	
+	
+	
+	
 }
