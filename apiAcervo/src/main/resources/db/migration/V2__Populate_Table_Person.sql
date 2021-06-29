3eadf8b226f2467fabf16057a1b47e0adb9022cc
pedro_ibs <pedro.igor.ifsp@gmail.com>
2021-06-28 22:15:39 -0300

    criando baser da API

---
+++ apiAcervo/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Vale da tranquilidade, Lua', 'Pedro Igor', 'Male', 'B. S.'),
+	(2, 'Vale da tranquilidade, Lua', 'T1000', 'Male', ' '),
+	(3, 'Vale da tranquilidade, Lua', 'Sara', 'Female', 'Conor'),
+	(4, 'Jupter', 'HAL', 'None', '9000');
